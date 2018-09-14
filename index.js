729547a49a324512e14ec06085310067f17e1cb9
third-meow <jsheaandcats@gmail.com>
2018-09-14 22:06:58 +1200

    fixed logging and syntax errors

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const PORT = 3080
 
 express()
-  .use(express.static(path.join(__dirname, 'public'))
+  .use(express.static(path.join(__dirname, 'public')))
   .get('/', (req, res) => res.render('index.html'))
-	.listen(PORT, () => console.log('Listening on ${ PORT }'))
+	.listen(PORT, () => console.log('Listening on 3080'))
