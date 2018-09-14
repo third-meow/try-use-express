c37ff16dd2c3f593c290bdbebef490a7bd42453f
third-meow <jsheaandcats@gmail.com>
2018-09-14 21:46:49 +1200

    added new public folder, moved index.html into it

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const express = require('express');
-const app = express();
+const PORT = 3080
 
-app.get('/', function(req, res){
-	res.sendFile(path.join(__dirname + '/index.html'))
-});
-
-app.listen(3080);
+express()
+  .use(express.static(path.join(__dirname, 'public'))
+  .get('/', (req, res) => res.render('index.html'))
+	.listen(PORT, () => console.log('Listening on ${ PORT }'))
