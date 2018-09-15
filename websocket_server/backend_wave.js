39de415f45139d11ebd7434c5ddb3d581b94b2e7
third-meow <jsheaandcats@gmail.com>
2018-09-16 11:41:24 +1200

    added websocket server basics

--- websocket_server/backend_wave.js
+++ websocket_server/backend_wave.js
@@ -1,2 +1,7 @@
 
-var ws = require("ws");
+const wSocket = require('ws');
+const wss = new wSocket.Server({ port: 3081 });
+
+wss.on('connection', function connection(ws) {
+  ws.send('good day');
+});
